ff1c0aa0697f11d43b6b95163f34e8b89fdf18f2
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-05 21:10:28 +0300

    Добавил метод get_parse для отработки исключений

--- API.py
+++ API.py
@@ -1,25 +1,29 @@
 import requests
 from pprint import pprint
 
-# username = 'dmitrprokofiev'
-# url = f"https://api.github.com/users/{username}/repos"
-# response = requests.get(url).json()
-#
-# result = [i['html_url'] for i in response]
-# print('\n'.join(result))
-
 class Repo:
     def __init__(self, username):
         self.username = username
 
     def parce(self):
         url = f"https://api.github.com/users/{self.username}/repos"
-        response = requests.get(url).json()
+        response = requests.get(url)
+        return response
+
+    def get_parse(self):
+        if self.parce().status_code == 200:
+            return self.parce()
+        else:
+            raise ValueError('Сервер не отвечает')
+
+    def result_parce(self):
+        response = self.get_parse().json()
         result = [i['html_url'] for i in response]
         return result
 
     def output(self):
-        print(f"У пользователя {self.username} имеется {len(self.parce())} репозиториев:{', '.join(self.parce())}")
+        print(f"У пользователя {self.username} имеется {len(self.result_parce())} репозиториев:{', '.join(self.result_parce())}")
               #TO DO добавить \n
+
 Dmitrii = Repo('dmitrprokofiev')
 Dmitrii.output()

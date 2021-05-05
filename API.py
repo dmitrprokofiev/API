944703ebd3f3f2bfb88c0505671f58b786014c6c
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-05 19:10:26 +0300

    init

---
+++ API.py
@@ -0,0 +1,8 @@
+import requests
+from pprint import pprint
+
+username = 'dmitrprokofiev'
+url = f"https://api.github.com/users/{username}/repos"
+response = requests.get(url).json()
+# 'html_url': 'https://github.com/dmitrprokofiev/API' - tag
+pprint(response[0]['html_url'])

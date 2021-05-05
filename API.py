396d6205389c218311e7b8bb6fc71159e671e335
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-05 19:47:46 +0300

    сделал удобочитаемый вывод

--- API.py
+++ API.py
@@ -6,5 +6,5 @@ url = f"https://api.github.com/users/{username}/repos"
 response = requests.get(url).json()
 # 'html_url': 'https://github.com/dmitrprokofiev/API' - tag
 # pprint(response[0]['html_url'])
-for i in response:
-    print(i['html_url'])
+result = [i['html_url'] for i in response]
+print('\n'.join(result))

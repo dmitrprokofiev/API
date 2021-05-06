56e850b2f500eb8a43ddbd30e48ddafc7838c86c
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-06 15:21:38 +0300

    Закончил код. Количество запросов израсходовал Протестить список репозиториев Apache

--- API.py
+++ API.py
@@ -1,5 +1,5 @@
 import requests
-from pprint import pprint
+import json
 
 class Repo:
     def __init__(self, username):
@@ -10,20 +10,26 @@ class Repo:
         response = requests.get(url)
         return response
 
-    def get_parse(self):
+    def get_parse(self): # проверка ответа от сервера
         if self.parce().status_code == 200:
             return self.parce()
         else:
             raise ValueError('Сервер не отвечает')
 
-    def result_parce(self):
+    def result_parce(self): # охраняем список репозиториев пользователя
         response = self.get_parse().json()
         result = [i['html_url'] for i in response]
         return result
 
-    def output(self):
+    def output(self): # выводим в удобочитаемый вид
         print(f"У пользователя {self.username} имеется {len(self.result_parce())} репозиториев:{', '.join(self.result_parce())}")
               #TO DO добавить \n
 
+    def save_json(self): # сохраняем в json-файл
+        with open('repo_{}.json'.format(self.username), 'w', encoding='utf-8') as js:
+            js.write(json.dumps(self.parce().json(), ensure_ascii=False))
+
 Dmitrii = Repo('dmitrprokofiev')
+# Apache = Repo('apache')
 Dmitrii.output()
+# Dmitrii.save_json()

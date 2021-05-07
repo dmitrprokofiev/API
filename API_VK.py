6447a06317a011a28a7de9f8a826f6b65e78753f
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-07 16:57:39 +0300

    добавил пару методов и получил с них данные

--- API_VK.py
+++ API_VK.py
@@ -1,11 +1,29 @@
 import requests
+from pprint import pprint
 
 class VK:
-    def __init__(self):
-        pass
+    _acces_token = '7ae4381313757f207145b586be86dde0190155f74ceef9ad1a230d7c8e52742e8927abe03693eb5bb611d'
 
+    def __init__(self, id):
+        self.id = id
 
-url = 'https://api.vk.com/method/users.get?user_ids=id205766304&fields=bdate&access_token=7ae4381313757f207145b586be86dde0190155f74ceef9ad1a230d7c8e52742e8927abe03693eb5bb611d&v=5.130'
-access_token = '7ae4381313757f207145b586be86dde0190155f74ceef9ad1a230d7c8e52742e8927abe03693eb5bb611d'
-response = requests.get(url)
-print(response.text)
+    def friends_get(self):
+        url = 'https://api.vk.com/method/friends.get?user_id={}&fields=bdate&access_token={}&v=5.130'.format(self.id, self._acces_token)
+        response = requests.get(url)
+        response = response.json()
+        for i in response['response']['items']:
+            print(i['first_name'], i['last_name'], i['id'])
+
+    def bannned(self): # черный список
+        url = 'https://api.vk.com/method/account.getBanned?user_id={}&fields=bdate&access_token={}&v=5.130'.format(self.id, self._acces_token)
+        response = requests.get(url)
+        response = response.json()
+        for i in response['response']['profiles']:
+            print(i['first_name'], i['last_name'])
+
+Buchkin = VK('46454373')
+Ageev = VK('134066584')
+Dmitrii = VK('320944047')
+# pprint(Buchkin.friends_get())
+# print(Buchkin.bannned())
+pprint(Dmitrii.friends_get())

4bb2016cd25872eb8eaed4f65245aafa7f4001d2
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-08 16:12:26 +0300

    init2

--- HH.py
+++ HH.py
@@ -3,9 +3,8 @@ import requests
 from pprint import pprint
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
-url = 'https://spb.hh.ru/search/vacancy?clusters=true&enable_snippets=true&salary=&st=searchVacancy&text=python'
+url = 'https://spb.hh.ru/search/vacancy?clusters=true&enable_snippets=true&salary=&st=searchVacancy&text=rust&showClusters=true&page=2'
 response = requests.get(url, headers=headers)
 dom = bs(response.text, "html.parser")
-s_list = dom.find_all('span', {'class' : 'resume-search-item__name'})
+s_list = dom.find_all(class_='resume-search-item__name')
 pprint(len(s_list))
-lk

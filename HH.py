dc49310aaae7ab321f2d79564a9fe2e47eb30a3a
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-08 12:03:06 +0300

    init

---
+++ HH.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup as bs
+import requests
+from pprint import pprint
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
+url = 'https://spb.hh.ru/search/vacancy?clusters=true&enable_snippets=true&salary=&st=searchVacancy&text=python'
+response = requests.get(url, headers=headers)
+dom = bs(response.text, "html.parser")
+s_list = dom.find_all('span', {'class' : 'resume-search-item__name'})
+pprint(len(s_list))
+lk

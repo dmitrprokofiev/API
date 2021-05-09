ea596066725e10f9933a24b10859fe596a831f3d
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-09 11:45:29 +0300

    добавил еще несколько методов и метод page

--- HH.py
+++ HH.py
@@ -3,16 +3,13 @@ import requests
 from pprint import pprint
 import pandas as pd
 
-# headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
-# url = 'https://spb.hh.ru/search/vacancy?clusters=true&enable_snippets=true&salary=&st=searchVacancy&text=rust&showClusters=true&page=1'
-# response = requests.get(url, headers=headers)
-# dom = bs(response.text, "html.parser")
-# s_list = dom.find_all(class_='resume-search-item__name')
-# post = [i.text for i in s_list]
-# link = [str(i).split()[5][6:-1] for i in s_list]
-# data_link = pd.DataFrame(link)
-# data_post = pd.DataFrame(post)
-# pprint(pd.concat([data_post, data_link], axis=1))
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
+url = 'https://spb.hh.ru/search/vacancy?clusters=true&enable_snippets=true&salary=&st=searchVacancy&text=rust&showClusters=true&page=1'
+response = requests.get(url, headers=headers)
+dom = bs(response.text, "html.parser")
+s_list = dom.find_all(class_='resume-search-item__name')
+pprint([i for i in s_list])
+
 
 class HH:
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
@@ -40,7 +37,23 @@ class HH:
         s_list = dom.find_all(class_='resume-search-item__name')
         return pd.DataFrame([i.text for i in s_list])
 
+    def get_link(self):
+        dom = bs(self.get_parce().text, "html.parser")
+        s_list = dom.find_all(class_='resume-search-item__name')
+        return pd.DataFrame([str(i).split()[5][6:-1] for i in s_list])
 
+    def page(self):
+        self.params['page'] = 2
+        response = requests.get(self.url, headers=self.headers, params=self.params)
+        return response
 
-rust = HH('rust')
-print(rust.get_post())
+    def pay(self):
+        pass
+
+    def df_view(self):
+        return pd.concat([self.get_post(), self.get_link()], axis=1)
+
+    def import_xls(self):
+        pass
+
+rust = HH()

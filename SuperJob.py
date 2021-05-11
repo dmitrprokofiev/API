987b0d27fdef0b4d2f8064114f197a9804c43ab1
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-11 18:18:41 +0300

    добавил метод get_link_href

--- SuperJob.py
+++ SuperJob.py
@@ -18,13 +18,14 @@ import openpyxl
 class SuperJob:
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 8.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
     url = 'https://russia.superjob.ru/vacancy/search/?keywords='
+    params = {'page' : None}
 
     def __init__(self, search):
         self.search = search
 
     def get_parce(self): # проверка на ответ о сервера
         while True:
-            response = requests.get(self.url+self.search, headers=self.headers)
+            response = requests.get(self.url+self.search, headers=self.headers, params=self.params)
             if response.status_code == 200:
                 return response
             time.sleep(0.5)
@@ -36,6 +37,15 @@ class SuperJob:
         k = ''.join(k[0])
         return int(k)
 
+    def get_link_href(self): # тот же метод что и get_link только через get('href')
+        result = []
+        for i in range(self.search_result() // 20 + 1):
+            self.params['page'] = i
+            dom = bs(self.get_parce().text, "html.parser")
+            page_list = dom.find_all('a', {'data-qa' : 'vacancy-serp__vacancy-title'})
+            for p in page_list:
+                result.append(p.get('href'))
+        return pd.DataFrame(result)
 
 kuznec = SuperJob('пескоструйщик')
 pprint(kuznec.search_result())

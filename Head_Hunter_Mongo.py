7f0c667d8d619cc05998755a2935ee631f03cd37
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-13 19:50:38 +0300

    добавил пару методов

--- Head_Hunter_Mongo.py
+++ Head_Hunter_Mongo.py
@@ -6,12 +6,8 @@ import pandas as pd
 import time
 from pymongo import MongoClient
 
-# как передать в params поисковый запрос!!!
+
 class HeadHunter:
-    #
-    # def __init__(self, search):
-    #     self.search = search
-    #     self.params['text'] = self.searchd
 
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
     url = 'https://hh.ru/search/vacancy?clusters=true'
@@ -114,7 +110,7 @@ class HeadHunter:
     def df_view(self):  # объединяет данные в твблицу
         data = pd.concat([pd.Series(self.parce_id()), pd.Series(self.get_post()), pd.Series(self.get_link_href()),
                           pd.Series(self.pay_min()), pd.Series(self.pay_min()), pd.Series(self.salary())], axis=1)
-        data.columns = ['_id', 'name', 'link', 'pay_min', 'pay_max', 'salary']
+        data.columns = ['_id', 'name', 'link', 'pay_min', 'pay_max', 'salary'] # называем столбцы
         return data
 
     def import_xls(self): # импортирует, полученные данные в файл xlsx
@@ -127,7 +123,7 @@ class HeadHunter:
         result = json.loads(self.df_view().T.to_json()).values()
         return result
 
-    def into_mongo(self):
+    def into_mongo(self): #метод добавляет в mongoDB только уникальные данные по id с сайта, который равен id в mongoDB
         for i in self.load_json():
             if i not in [i for i in self.persons.find({})]:
                 self.persons.insert_one(i)

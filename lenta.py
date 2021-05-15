29719b0ba549e4e3e9f5a4e8929a01ff8a5933be
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-15 16:30:13 +0300

    init1

--- lenta.py
+++ lenta.py
@@ -10,9 +10,9 @@ news = []
 elements = dom.xpath("//div[@class='span4']/section/div")
 for el in elements:
     new = {}
-    names = el.xpath("..//div[@class='span4']/section[1]//div[@class='titles']//span//text()")
-    link = el.xpath("..//div[@class='span4']/section[1]//div[@class='titles']//@href")
-    new['date'] = el.xpath("..//div[@class='span4']/section[1]//div[@class='info g-date item__info']//span/span[1]//text()")
+    names = el.xpath(".//div[@class='span4']/section[1]//div[@class='titles']//span//text()")
+    link = el.xpath(".//div[@class='span4']/section[1]//div[@class='titles']//@href")
+    new['date'] = el.xpath(".//div[@class='span4']/section[1]//div[@class='info g-date item__info']//span/span[1]//text()")
     new['names'] = [url+i if 'lenta' not in i else i for i in link]
     new['link'] = [i.replace('\xa0', '') for i in names]
 

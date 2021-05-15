ac91a8ebdef78b70b0e12ecccf5e3a9248f7bd87
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-15 19:57:05 +0300

    добавил метод into_mongo

--- lenta.py
+++ lenta.py
@@ -1,23 +1,38 @@
 import requests
 from lxml import html
+from pymongo import MongoClient
 from pprint import pprint
 
 headers = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'}
 url = 'https://lenta.ru'
 response = requests.get(url, headers=headers)
 dom = html.fromstring(response.text)
-news = []
-elements = dom.xpath("//div[@class='span4']/section/div")
-for el in elements:
-    new = {}
-    names = el.xpath(".//div[@class='titles']//text()")
-    link = el.xpath(".//div[@class='titles']//@href")
-    new['date'] = el.xpath(".//span/span[1]//text()")
-    new['link'] = [url+i if 'lenta' not in i else i for i in link]
-    new['names'] = [i.replace('\xa0', '') for i in names]
 
-    news.append(new)
+client = MongoClient('127.0.0.1', 27017)
+db = client['news_lenta']
+persons = db.persons
 
-pprint(news)
+def parce_news():
+    news = []
+    elements = dom.xpath("//div[@class='span4']/section/div")
+    for el in elements:
+        new = {}
+        names = el.xpath(".//div[@class='titles']//text()")
+        link = el.xpath(".//div[@class='titles']//@href")
+        new['date'] = [i for i in el.xpath(".//span/span[1]//text()")]
+        new['link'] = [url+i if 'lenta' not in i else i for i in link]
+        new['names'] = [i.replace('\xa0', '') for i in names]
+        news.append(new)
+    return news
+
+def into_mongo(into):
+    for i in into:
+        if i not in [s for s in persons.find({})]:
+            persons.insert_one(i)
+
+# parcing = parce_news()
+# into_mongo(parcing)
+
+pprint([person for person in persons.find({})])
 
 

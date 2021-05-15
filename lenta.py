4b31b76e2d1574fc19d884820a699fb79818fc53
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-15 18:51:18 +0300

    получил список словарей

--- lenta.py
+++ lenta.py
@@ -7,27 +7,17 @@ url = 'https://lenta.ru'
 response = requests.get(url, headers=headers)
 dom = html.fromstring(response.text)
 news = []
-# elements = dom.xpath("//div[@class='span4']/section/div")
-# for el in elements:
-#     new = {}
-#     names = el.xpath(".//div[@class='span4']/section[1]//div[@class='titles']//span//text()")
-#     link = el.xpath(".//div[@class='span4']/section[1]//div[@class='titles']//@href")
-#     new['date'] = el.xpath(".//div[@class='span4']/section[1]//div[@class='info g-date item__info']//span/span[1]//text()")
-#     new['names'] = [url+i if 'lenta' not in i else i for i in link]
-#     new['link'] = [i.replace('\xa0', '') for i in names]
-#
-#     news.append(new)
-#
-# pprint(news)
-
-elements = dom.xpath("//div[@class='span4']/section[1]//div")
+elements = dom.xpath("//div[@class='span4']/section/div")
 for el in elements:
+    new = {}
+    names = el.xpath(".//div[@class='titles']//text()")
+    link = el.xpath(".//div[@class='titles']//@href")
+    new['date'] = el.xpath(".//span/span[1]//text()")
+    new['link'] = [url+i if 'lenta' not in i else i for i in link]
+    new['names'] = [i.replace('\xa0', '') for i in names]
+
+    news.append(new)
+
+pprint(news)
 
 
-names = dom.xpath("//div[@class='titles']//span//text()")
-link = dom.xpath("//div[@class='titles']//@href")
-date = dom.xpath("//div[@class='info g-date item__info']//span/span[1]//text()")
-link = [url+i if 'lenta' not in i else i for i in link]
-names = [i.replace('\xa0', '') for i in names]
-
-pprint(elements)

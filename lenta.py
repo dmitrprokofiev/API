2ef4e992157ad82088e423c011d303dd98a22909
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-15 16:36:59 +0300

    init2

--- lenta.py
+++ lenta.py
@@ -7,15 +7,27 @@ url = 'https://lenta.ru'
 response = requests.get(url, headers=headers)
 dom = html.fromstring(response.text)
 news = []
-elements = dom.xpath("//div[@class='span4']/section/div")
+# elements = dom.xpath("//div[@class='span4']/section/div")
+# for el in elements:
+#     new = {}
+#     names = el.xpath(".//div[@class='span4']/section[1]//div[@class='titles']//span//text()")
+#     link = el.xpath(".//div[@class='span4']/section[1]//div[@class='titles']//@href")
+#     new['date'] = el.xpath(".//div[@class='span4']/section[1]//div[@class='info g-date item__info']//span/span[1]//text()")
+#     new['names'] = [url+i if 'lenta' not in i else i for i in link]
+#     new['link'] = [i.replace('\xa0', '') for i in names]
+#
+#     news.append(new)
+#
+# pprint(news)
+
+elements = dom.xpath("//div[@class='span4']/section[1]//div")
 for el in elements:
-    new = {}
-    names = el.xpath(".//div[@class='span4']/section[1]//div[@class='titles']//span//text()")
-    link = el.xpath(".//div[@class='span4']/section[1]//div[@class='titles']//@href")
-    new['date'] = el.xpath(".//div[@class='span4']/section[1]//div[@class='info g-date item__info']//span/span[1]//text()")
-    new['names'] = [url+i if 'lenta' not in i else i for i in link]
-    new['link'] = [i.replace('\xa0', '') for i in names]
 
-    news.append(new)
 
-pprint(news)
+names = dom.xpath("//div[@class='titles']//span//text()")
+link = dom.xpath("//div[@class='titles']//@href")
+date = dom.xpath("//div[@class='info g-date item__info']//span/span[1]//text()")
+link = [url+i if 'lenta' not in i else i for i in link]
+names = [i.replace('\xa0', '') for i in names]
+
+pprint(elements)

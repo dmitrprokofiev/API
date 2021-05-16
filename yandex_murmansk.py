f8ea28c2af330dbc4173a65c212b8c4f3c859dc5
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-16 18:42:12 +0300

    добавил парру методов

--- yandex_murmansk.py
+++ yandex_murmansk.py
@@ -3,35 +3,47 @@ from lxml import html
 from pymongo import MongoClient
 from pprint import pprint
 
-class Yandex_Parce:
-    headers = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'}
-    url = "https://yandex.ru/news"
-
-    def __init__(self, region):
-        self.region = region
-
-    def go(self):
-        self.url = self.url+"/region/"+self.region
-        response = requests.get(self.url, headers=self.headers)
-        if response.status_code == 200:
-            return html.fromstring(response.text)
-        else:
-            raise ValueError
-
+# class Mail_News:
+#     headers = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'}
+#     url = "https://news.mail.ru/"
+#
+#
+#     def go(self):
+#         response = requests.get(self.url, headers=self.headers)
+#         if response.status_code == 200:
+#             return html.fromstring(response.text)
+#         else:
+#             raise ValueError
+#
 #     def go_parce(self):
 #         news = []
-#         elements = self.go().xpath("//div[@class='mg-grid__col mg-grid__col_xs_8']")
-#         return elements
+#         elements = self.go().xpath("//ul//li")
+#         for el in elements:
+#             new= {}
+#             new['name'] = ''.join([i.replace('\xa0', '') for i in el.xpath(".//text()")])
+#             new['link'] = ''.join([i for i in el.xpath(".//@href")])
+#             new['_id'] = ''.join([i for i in new['link'] if i.isdigit()])
+#
+#             news.append(new)
+#         return news
 #
 #
-# murmansk = Yandex_Parce('murmansk')
-# pprint(murmansk.go())
+# search = Mail_News()
+# pprint(search.go())
 
 headers = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'}
 url = "https://news.mail.ru/"
 response = requests.get(url, headers=headers)
 dom = html.fromstring(response.text)
+news = []
 elements = dom.xpath("//ul//li")
-name = dom.xpath("//ul/li//text()")
+for el in elements:
+    new= {}
+    new['name'] = ''.join([i.replace('\xa0', '') for i in el.xpath(".//text()")])
+    new['link'] = ''.join([i for i in el.xpath(".//@href")])
+    new['_id'] = ''.join([i for i in new['link'] if i.isdigit()])
+
+    news.append(new)
 pprint(len(elements))
-pprint(len(name))
+# pprint(len(name))
+pprint(news)

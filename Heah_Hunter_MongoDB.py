e7e86237d0c79f80011b42448c514361e57d03c0
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-23 20:17:37 +0300

    извлек зарплаты

--- Heah_Hunter_MongoDB.py
+++ Heah_Hunter_MongoDB.py
@@ -1,4 +1,5 @@
 from Head_Hunter import HeadHunter
+from pprint import pprint
 
 class Mongo_Hunter(HeadHunter):
     def add_mongo(self):
@@ -11,9 +12,24 @@ class Mongo_Hunter(HeadHunter):
             mongo['name'] = p.find('a', {'class' : 'bloko-link'}).text
             mongo['_id'] = int(mongo['link'].split('/')[-1])
 
+            try:
+                pay = p.find('span', {'data-qa' : 'vacancy-serp__vacancy-compensation'}).text.replace('\u202f', '')
+                if pay.split()[0].isdigit():
+                    mongo["pay_min"] = int(pay.split()[0])
+                    mongo["pay_max"] = int(pay.split()[2])
+                    mongo["currency"] = pay.split()[3]
+                else:
+                    mongo["pay_min"] = int(pay.split()[1])
+                    mongo["pay_max"] = mongo["pay_min"]
+                    mongo["currency"] = pay.split()[2]
+            except:
+                mongo["pay_min"] = None
+                mongo["pay_max"] = None
+                mongo["currency"] = None
+
             mongoDB.append(mongo)
         return mongoDB
 
 
 head = Mongo_Hunter()
-print(head.add_mongo())
+pprint(head.add_mongo())

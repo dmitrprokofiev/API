cd9fccd1485e82efec04e6e8d5a747975e5950af
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-28 19:59:20 +0300

    начал обратотку item hh.ru

--- jobparser/pipelines.py
+++ jobparser/pipelines.py
@@ -15,7 +15,6 @@ class JobparserPipeline:
         self.mongobase = client.vacancy999
 
     def process_item(self, item, spider):
-        # Здесь обработка item'a
         if spider.name == 'superjob':
             item['_id'] = int(''.join([i for i in item['link'].split('-')[-1] if i.isdigit()]))
             if len(item['salary']) == 3:
@@ -23,25 +22,34 @@ class JobparserPipeline:
                     item['min_salary'] = int(''.join([i for i in item['salary'][2] if i.isdigit()]))
                     item['max_salary'] = None
                     item['currency'] = ''.join([i for i in item['salary'][2] if i.isalpha()])
-                    del item["salary"]
                 else:
                     item['min_salary'] = None
                     item['max_salary'] = int(''.join([i for i in item['salary'][2] if i.isdigit()]))
                     item['currency'] = ''.join([i for i in item['salary'][2] if i.isalpha()])
-                    del item["salary"]
             elif len(item['salary']) > 3:
                 item['min_salary'] = int(''.join([i for i in item['salary'][0] if i.isdigit()]))
                 item['max_salary'] = int(''.join([i for i in item['salary'][1] if i.isdigit()]))
                 item['currency'] = ''.join([i for i in item['salary'][3] if i.isalpha()])
-                del item["salary"]
             else:
                 item['min_salary'] = None
                 item['max_salary'] = None
                 item['currency'] = None
-                del item["salary"]
+            del item["salary"]
+        elif spider.name == "hhru":
+            # item['_id'] = int(item['link'].split('/')[4].split('?')[0])
+            # if len(item["salary"]) > 6:
+            #     item['min_salary'] = int(item['salary'][1].replace('\xa0', ''))
+            #     item['max_salary'] = int(item['salary'][3].replace('\xa0', ''))
+            #     item['currency'] = ''.join([i for i in item['salary'][5] if i.isalpha()])
+            # elif len(item["salary"]) == 4:
 
+            # else:
+            #     item['min_salary'] = None
+            #     item['max_salary'] = None
+            #     item['currency'] = None
+            # del item["salary"]
             return item
-
+        print()
 
         # collection = self.mongobase[spider.name]
         # collection.insert_one(item)
